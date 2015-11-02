41f75cd4de4622efc9b99c4f5df877830d07efda
Alex White <awhit012@gmail.com>
2015-11-01 16:31:07 -0800

    Update solutions.rb

--- w06-ruby/w06-d1-dusk-intro-ruby/solutions/solutions.rb
+++ w06-ruby/w06-d1-dusk-intro-ruby/solutions/solutions.rb
@@ -1,5 +1,5 @@
 # Basic Solution to Exercise 1
-def pTimes(statement,num)
+def p_times(statement,num)
   i = 0
   while i < num do
     puts statement
@@ -8,31 +8,43 @@ def pTimes(statement,num)
 end
 
 # A spiffy way to do it
-def pTimes2(statement,num)
+def p_times2(statement,num)
   num.times do
     puts statement
   end
 end
 
-# pTimes('hello',5)
+# p_times('hello',5)
 
 # Solution to Exercise 2
-def letterCount(str)
-  ans = {}
+def letter_count string
+  frequency = {}
   i = 0
-  while i < str.length do
-    if ans[str[i]] == nil
-      ans[str[i]] = 1
+  while i < string.length do
+    if !frequency[string[i]]
+      frequency[string[i]] = 1
     else
-      ans[str[i]] += 1
+      frequency[string[i]] += 1
     end
     i += 1
   end
-  ans
+  frequency
+end
+
+# Fancier solution to Exercise 2 using .each_char
+def letter_count string
+	frequency = {}
+	string.each_char do |char|
+		if frequency[char]
+			frequency[char] += 1
+		else frequency[char] = 1
+		end
+	end
+	frequency
 end
 
 # puts letterCount 'hello'
-def mockMe
+def mock_me
   while true do
     mock = gets.chomp
     if mock == 'quit'
@@ -43,20 +55,20 @@ def mockMe
   end
 end
 
-# mockMe
+# mock_me
 
 # Exercise 4
-def printContacts(contacts)
+def print_contacts(contacts)
   contacts.each do |k,v,l|
     puts k, v
   end
 end
 
-contactsHash = {'michael' => '555-555-5555', 'del' => '666-666-6666'}
-# printContacts contactsHash
+contacts_hash = {'michael' => '555-555-5555', 'del' => '666-666-6666'}
+# print_contacts contactsHash
 
 # Exercise 5
-def getContact(contacts)
+def get_contact(contacts)
   puts contacts
   puts 'Name: '
   name = gets.chomp
@@ -66,18 +78,18 @@ def getContact(contacts)
   contacts
 end
 
-# puts getContact contactsHash
+# puts get_contact contacts_hash
 
 ## List exercises
-def getSum(xs)
+def get_sum(xs)
   xs.inject do |memo,x|
     memo + x
   end
 end
 
-# puts getSum [1,2,3]
+# puts get_sum [1,2,3]
 
-def getMax(xs)
+def get_max(xs)
   xs.inject do |memo,x|
     if memo < x
       x
@@ -87,9 +99,9 @@ def getMax(xs)
   end
 end
 
-# puts getMax [1,2,5,4,3]
+# puts get_max [1,2,5,4,3]
 
-def getMin(xs)
+def get_min(xs)
   xs.inject do |memo,x|
     if memo < x
       memo
@@ -99,17 +111,17 @@ def getMin(xs)
   end
 end
 
-# puts getMin [1,2,-2,5,4,3]
+# puts get_min [1,2,-2,5,4,3]
 
-def reverseStr(str)
+def reverse_str(str)
   str.chars.inject do |memo, x|
     x + memo
   end
 end
 
-# puts reverseStr "hello world"
+# puts reverse_str "hello world"
 
-def partialSums(arr)
+def partial_sums(arr)
   sums = [0]
  arr.inject(0) do |memo,x|
     sums.push(memo+x)
@@ -118,30 +130,30 @@ def partialSums(arr)
  sums
 end
 
-# p partialSums [1,2,3]
+# p partial_sums [1,2,3]
 
-def multiplyBy(num,arr)
+def multiply_by(num,arr)
   arr.map do |x|
     x * num
   end
 end
 
-def reverseEach(arr)
+def reverse_each(arr)
   arr.map do |x|
-    reverseStr x
+    reverse_str x
   end
 end
 
-# p reverseEach ['hello','world','michael']
+# p reverse_each ['hello','world','michael']
 
-def getResponses(arr)
+def get_responses(arr)
   arr.map do |x|
     puts x
     gets.chomp
   end
 end
 
-# p getResponses ['what','the','fuck']
+# p get_responses ['what','the','fuck']
 
 def factorial(n)
   if n < 2
