fa33b9d4749e912507acc49939e52ac88b69f206
Brianna Veenstra <bgveenstra@users.noreply.github.com>
2015-11-04 10:37:15 -0800

    blank out monster.rb starter

--- w06-ruby/d3-dawn-oop/monster.rb
+++ w06-ruby/d3-dawn-oop/monster.rb
@@ -1,118 +1,44 @@
 class Monster
   # getters and setters for instance variables
-  attr_accessor :threat_level, :habitat, :name
 
-  # class variable
-  @@count = 0
+  # class variable for count
 
-  # class instance variable
-  @class_description = "A scary monster!"
+  # class instance variable for class description
 
   # class method getter for @@count class variable
-  def self.count
-    @@count
-  end
 
   # class method getter for @class_description class instance variable
-  def self.class_description
-    @class_description
-  end
 
   # initial behavior
-  def initialize(threat_level=:medium)
-    @threat_level = threat_level
-    puts "Rawr!"
-    @@count = @@count + 1
-    puts "#{@@count} monsters now roam the world!"
-  end
 
-  # instance method
-  def habitat?(some_hab)
-    @habitat == some_hab
-  end
+  # habitat? instance method
   
-  # instance method
-  def get_dangerous
-    # ruby version of a switch statment is case
-    case @threat_level
-      when :low
-        @threat_level = :medium
-      when :medium
-        @threat_level = :high
-      when :high
-        @threat_level = :midnight
-      when :midnight
-        :midnight
-    end
-  end
+  # get_dangerous instance method
+  
 
-  # class method
-  def self.fight(monster1, monster2)
-    if monster1.threat_level == monster2.threat_level
-      monster2
-    elsif monster1.threat_level == :low
-      monster2
-    elsif monster1.threat_level == :medium
-      if monster2.threat_level == :low
-        monster1
-      else
-        monster2
-      end
-    else # monster1.threat_level is :high or :midnight
-      if monster2.threat_level == :midnight
-        monster2
-      else
-        monster1
-      end
-    end
-  end
-end
+  # fight class method
 
-
-class Zombie < Monster
+# Zombie class
 
   # zombie version of class_description
-  @class_description = "Shuffling, decaying flesh. Hungry and without remorse!"
+  
 
-  # we'll use the reserved word super
-  # to call Monster's initialize and keep our code more DRY
-  def initialize (threat_level=:medium)
-    # note that zombies created without a threat level
-    # will still have a default threat_level of :medium
-    super(threat_level)  
-    @habitat = "graveyard"
-  end
-end
+  # initial behavior and values
 
-class Werewolf < Monster
+# Werewolf class
 
   # werewolf version of class_description
-  @class_description = "A man... a wolf... a monster!"
 
-  def initialize (threat_level=:low)
-    # werewolves created without a threat level
-    # will have a default threat_level of :low
-    super(threat_level)  
-  end
+  # initial behavior and values 
+  
+  # update_threat_level instance method
 
-  def update_threat_level(full_moon)
-    if full_moon
-      @threat_level = :midnight
-    else
-      @threat_level = :low
-    end
-    @threat_level
-  end
-end
+# Flying module
 
+  #fly method
 
-module Flying
-  def fly
-    puts "#{self.name || "it"} soars through the air"
-  end
-end
+# Vampire class
 
-class Vampire < Monster
-  @class_description = "Dark and sparly."
-  include Flying
-end
+  # vampire class description
+  
+  # Flying module included
