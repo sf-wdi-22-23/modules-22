0f25fd3dad8dad60ca935a90587590e3b7bd192a
Brianna Veenstra <b.g.veenstra@gmail.com>
2015-11-10 10:45:23 -0800

    rm extra tests

--- w07-rails/d2-dawn-auth/PracticeAuth/app/controllers/users_controller.rb
+++ w07-rails/d2-dawn-auth/PracticeAuth/app/controllers/users_controller.rb
@@ -18,7 +18,7 @@ class UsersController < ApplicationController
     end
 
     def show
-    	@user = current_user
+    	@user = @current_user
     end
 
     private
--- w07-rails/d2-dawn-auth/PracticeAuth/spec/controllers/users_controller_spec.rb
+++ w07-rails/d2-dawn-auth/PracticeAuth/spec/controllers/users_controller_spec.rb
@@ -9,13 +9,6 @@ RSpec.describe UsersController, type: :controller do
     end
   end
 
-  describe "GET #create" do
-    it "returns http success" do
-      get :create
-      expect(response).to have_http_status(:success)
-    end
-  end
-
   describe "GET #show" do
     it "returns http success" do
       get :show
